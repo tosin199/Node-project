66bbaea842dea0c063ba49c7be3058012f1c5703
Lydia Tosin <olawoyelydiatosin@gmail.com>
2020-10-30 08:25:15 -0700

    modifying assets and index.html

--- assets/js/object.js
+++ assets/js/object.js
@@ -1,13 +1,15 @@
-var person = new Object();
-person.firstName = "Excellency";
-person.lastName = "Tosin";
-person.age = 40;
-person.complexion = "light";
-document.getElementById("description").innerHTML = person.firstName + person.lastName + "is" + person.age + "years old" + "and" + person.complexion + "in complexion.";
-var http = request ('http');
-var url = require('url');
-http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type'; 'text/index.html'});
-})
-server.listen(8081, '127.0.0.1');
-console.log('Hey, now listening to port 8081');
+var person= {
+firstName = "Excellency",
+lastName = "Tosin",
+age = 40,
+complexion = "light",
+};
+alert(person.firstName + person.lastName + "is" + person.age + "years old" + "and" + person.complexion + "in complexion.");
+
+// var http = request ('http');
+// var url = require('url');
+// http.createServer(function(req,res){
+//     res.writeHead(200, {'Content-Type'; 'text/index.html'});
+// })
+// server.listen(8081, '127.0.0.1');
+// console.log('Hey, now listening to port 8081');
