4a5291a57da57fde4f6fa7cc97d76ef57525d7bb
Lydia Tosin <olawoyelydiatosin@gmail.com>
2020-10-30 09:25:57 -0700

    modifying index.html,creating object.js

--- js/object.js
+++ js/object.js
@@ -1,3 +1,12 @@
+var http = require ('http');
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {'COntent-Type':'text/plain'});
+    res.end('Javascript practice on function,object,json,class and callback');
+});
+
+server.listen(8081,'127.0.0.1');
+console.log('Hey Javascript, now listening to port 8081');
+
 var person= {
     firstName = "Excellency",
     lastName = "Tosin",
@@ -5,3 +14,6 @@ var person= {
     };
     Object.defineProperty(person, "language", {value:"light"})
     document.getElementById("demo").innerHTML = person.language;
+
+    
+    
