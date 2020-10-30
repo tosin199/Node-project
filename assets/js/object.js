107b27a9510bf4f8440c19125c7608d47e151e7a
Lydia Tosin <olawoyelydiatosin@gmail.com>
2020-10-30 09:15:27 -0700

    modifying index.html,creating object.js

--- assets/js/object.js
+++
@@ -1,7 +0,0 @@
-var person= {
-firstName = "Excellency",
-lastName = "Tosin",
-language = "Swahili",
-};
-Object.defineProperty(person, "language", {value:"light"})
-document.getElementById("demo").innerHTML = person.language;
---
+++ js/object.js
@@ -0,0 +1,7 @@
+var person= {
+    firstName = "Excellency",
+    lastName = "Tosin",
+    language = "Swahili",
+    };
+    Object.defineProperty(person, "language", {value:"light"})
+    document.getElementById("demo").innerHTML = person.language;
