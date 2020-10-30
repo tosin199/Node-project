a0738b8f4e700309816d136e02213e82094dcc69
Lydia Tosin <olawoyelydiatosin@gmail.com>
2020-10-30 09:04:58 -0700

    modifying assets/js/object.js

--- assets/js/object.js
+++ assets/js/object.js
@@ -1,15 +1,7 @@
 var person= {
 firstName = "Excellency",
 lastName = "Tosin",
-age = 40,
-complexion = "light",
+language = "Swahili",
 };
-alert(person.firstName + person.lastName + "is" + person.age + "years old" + "and" + person.complexion + "in complexion.");
-
-// var http = request ('http');
-// var url = require('url');
-// http.createServer(function(req,res){
-//     res.writeHead(200, {'Content-Type'; 'text/index.html'});
-// })
-// server.listen(8081, '127.0.0.1');
-// console.log('Hey, now listening to port 8081');
+Object.defineProperty(person, "language", {value:"light"})
+document.getElementById("demo").innerHTML = person.language;
