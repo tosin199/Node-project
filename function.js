125a9b374e73e7ca0e995350d0aa0c5b44cb6e89
Lydia Tosin <olawoyelydiatosin@gmail.com>
2020-10-30 10:09:44 -0700

    creating function.html and function.js

---
+++ function.js
@@ -0,0 +1,6 @@
+var x =function (a, b) {return a * b };
+document.getElementById("demo").innerHTML = x(4, 3);
+
+// var x =function (a, b) {return arguments.length};
+// document.getElementById("demo").innerHTML = x(4, 3);
+
